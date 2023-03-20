7a3e10a7a526d65e684c2e5d77940c6d20dec64f
subject026 <subject026@protonmail.com>
2023-03-20 10:25:02 +0000

    whoops didn't build

---
+++ packages/test-theme/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getBoopy = void 0;
+function getBoopy() {
+    return "bloopy!";
+}
+exports.getBoopy = getBoopy;
